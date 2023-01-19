2d40f860bf4258437b60ba97a57be092621ee469
pnb <peter.bajcsy@nist.gov>
2023-01-19 12:53:30 -0500

    added support for image histogram equalization for color images

--- src/histogram_equalization.py
+++ src/histogram_equalization.py
@@ -5,7 +5,7 @@
 import os
 import numpy as np
 import cv2 as cv
-# from matplotlib import pyplot as plt # this is for debugging
+from matplotlib import pyplot as plt # this is for debugging
 
 '''
 the method for applying image histogram equalization to one image
@@ -20,36 +20,70 @@ Published methods have been extended to work on 16 BPP images
 def hist_equal(img_file, input_dir, output_dir):
     img_filename = os.path.join(input_dir, img_file)
     save_filename = os.path.join(output_dir, img_file)
-    img = cv.imread(img_filename, cv.IMREAD_ANYDEPTH)
-    print('INFO: img type =', img.dtype)
-
-    equ = None
-    if img.dtype == 'uint8':
-        # the case of 8BPP
-        equ = cv.equalizeHist(img) #works only for 8BPP
-        # hist, bins = np.histogram(img.flatten(), 256, [0, 256])
-        # cdf = hist.cumsum()
-        # cdf_normalized = cdf * float(hist.max()) / cdf.max()
-        # # plt.plot(cdf_normalized, color='b')
-        # # plt.hist(img.flatten(), 256, [0, 256], color='r')
-        # # plt.xlim([0, 256])
-        # # plt.legend(('cdf', 'histogram'), loc='upper left')
-        # # plt.show()
-        # # Now we have the look-up table...
-        # equ = cdf_normalized[img]
-
-    if img.dtype == 'uint16':
-        # the case of 16 BPP
-        hist, bins = np.histogram(img.flatten(), 65536, [0, 65536])  # Collect 16 bits histogram (65536 = 2^16).
-        cdf = hist.cumsum()
-        cdf_m = np.ma.masked_equal(cdf, 0)  # Find the minimum histogram value (excluding 0)
-        cdf_m = (cdf_m - cdf_m.min()) * 65535 / (cdf_m.max() - cdf_m.min())
-        cdf = np.ma.filled(cdf_m, 0).astype('uint16')
-        # Now we have the look-up table...
-        equ = cdf[img]
+    img = cv.imread(img_filename, cv.IMREAD_UNCHANGED) #  cv.IMREAD_ANYDEPTH; cv.IMREAD_UNCHANGED - Read Image with Transparency Channel
+    print('Image size:', img.size)
+    print(f'dtype: {img.dtype}, shape: {img.shape}, min: {np.min(img)}, max: {np.max(img)}')
 
     if img.dtype != 'uint8' and img.dtype != 'uint16':
         print('ERROR: unsupported input image format')
+        return
+
+    equ = None
+    # grayscale/single channel support
+    if len(img.shape) < 3: # contains #rows and #cols
+        if img.dtype == 'uint8':
+            # the case of 8BPP
+            equ = cv.equalizeHist(img) #works only for 8BPP
+            # hist, bins = np.histogram(img.flatten(), 256, [0, 256])
+            # cdf = hist.cumsum()
+            # cdf_normalized = cdf * float(hist.max()) / cdf.max()
+            # # plt.plot(cdf_normalized, color='b')
+            # # plt.hist(img.flatten(), 256, [0, 256], color='r')
+            # # plt.xlim([0, 256])
+            # # plt.legend(('cdf', 'histogram'), loc='upper left')
+            # # plt.show()
+            # # Now we have the look-up table...
+            # equ = cdf_normalized[img]
+
+        if img.dtype == 'uint16':
+            # the case of 16 BPP
+            hist, bins = np.histogram(img.flatten(), 65536, [0, 65536])  # Collect 16 bits histogram (65536 = 2^16).
+            cdf = hist.cumsum()
+            cdf_m = np.ma.masked_equal(cdf, 0)  # Find the minimum histogram value (excluding 0)
+            cdf_m = (cdf_m - cdf_m.min()) * 65535 / (cdf_m.max() - cdf_m.min())
+            cdf = np.ma.filled(cdf_m, 0).astype('uint16')
+            # Now we have the look-up table...
+            equ = cdf[img]
+    elif len(img.shape) == 3:
+        # width = img.shape[0]
+        # height = img.shape[1]
+        # channels = img.shape[2]
+        # print('INFO: width=', width, ' height=', height, ' channels=', channels)
+
+        # color/3 channel support
+        if img.dtype == 'uint8':
+            # the case of 8BPP
+            hsv_img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+            ##### debug
+            # save_filename2 = os.path.join(output_dir, str('debugHSV_'+img_file))
+            # cv.imwrite(save_filename2, hsv_img)
+            # rgb_img = cv.cvtColor(hsv_img, cv.COLOR_HSV2BGR) # cv.COLOR_HSV2RGB; cv.COLOR_HSV2BGR
+            # save_filename3 = os.path.join(output_dir, str('debugRGB_'+img_file))
+            # cv.imwrite(save_filename3, rgb_img)
+
+            v_img = hsv_img[:, :, 2]
+            # save_filename4 = os.path.join(output_dir, str('debugHvalue_'+img_file))
+            # cv.imwrite(save_filename4, v_img)
+
+            v_equ = cv.equalizeHist(v_img)  # works only for 8BPP
+            # save_filename5 = os.path.join(output_dir, str('debugHvalueEQU_'+img_file))
+            # cv.imwrite(save_filename5, v_equ)
+
+            hsv_img[:,:, 2] = v_equ;
+            equ = cv.cvtColor(hsv_img, cv.COLOR_HSV2BGR) # cv.COLOR_HSV2RGB; cv.COLOR_HSV2BGR
+
+    else:
+        print('ERROR: unsupported number of channels')
         return
 
     cv.imwrite(save_filename, equ)
@@ -95,6 +129,9 @@ def main():
                         help='directory with tif image files (Required)', required=True)
     parser.add_argument('--output', dest='output_dir', type=str,
                         help='output directory where updated image files are saved (Required)', required=True)
+    parser.add_argument('--suffix', dest='image_suffix', type=str,
+                        help='image file suffix in the input directory (Optional)', required=False, default=None)
+
 
     args = parser.parse_args()
     if args.input_dir is None:
@@ -108,8 +145,13 @@ def main():
     input_dir = args.input_dir
     output_dir = args.output_dir
 
+    image_suffix = '.tif'
+    if args.image_suffix is not None:
+        image_suffix = args.image_suffix
+
     print('Arguments:')
     print('input input_dir = {}'.format(input_dir))
+    print('input image_suffix = {}'.format(image_suffix))
     print('output_dir = {}'.format(output_dir))
 
     # sanity checks
@@ -125,7 +167,7 @@ def main():
         print('ERROR: input_dir=', input_dir, ' is not a directory')
         return
 
-    image_suffix = '.tif'
+
     file_list = []
     for t_file in os.listdir(input_dir):
         # check for only files with the expected suffix
@@ -140,7 +182,7 @@ def main():
     print('INFO: list of files =', file_list)
     for file_name in file_list:
         # hist_eq(file_name, input_dir, output_dir)
-        #hist_equal_CLAHE(file_name, input_dir, output_dir)
+        # hist_equal_CLAHE(file_name, input_dir, output_dir)
         hist_equal(file_name, input_dir, output_dir)
 
 
